20af561fafadf7c300b63ce9bb7fdf755fc3d0a7
pablosecuen <pablosecuen@gmail.com>
2023-11-24 09:42:38 -0300

    seccion nosotros

--- client/app/(...)/nosotros/page.tsx
+++ client/app/(...)/nosotros/page.tsx
@@ -1,44 +1,140 @@
 import Image from "next/image";
 import React from "react";
-import scramble from "@/public/assets/svg/scramble.svg";
+import reactangle from "@/public/assets/svg/Rectangle.png";
+import rectangle1 from "@/public/assets/svg/Rectangle-1.png";
+import rectangle2 from "@/public/assets/svg/Rectangle-2.png";
+import rectangle3 from "@/public/assets/svg/Rectangle-3.png";
+import highlight from "@/public/assets/svg/highlight.svg";
+import label from "@/public/assets/svg/Label.png";
+
+import group from "@/public/assets/svg/group.png";
 import "./perspective.css";
+import { Banner } from "@/app/components/footer/banner";
 const Nosotros = () => {
   return (
-    <>
-      <section className="w-full min-h-screen pt-20 relative">
-        <div className="w-full h-[80vh] flex border">
-          <div className="w-1/2 h-full flex flex-col items-end justify-center  order-1 border">
-            {" "}
-            <span className="font-bold text-3xl w-[650px] pb-10">NOSOTROS</span>
-            <h3 className="text-3xl perspective-container max-w-[650px] font-bold leading-loose pb-2">
-              {/*          <Image
-              src={scramble}
-              alt="scramble"
-              width={150}
-              height={0}
-              className="absolute -top-8 right-20"
-            /> */}
-              “ Transformando Sueños en Palabras, Palabras en Poder: <br />{" "}
-              <div className="flex ">
-                creating{" "}
-                <div className="text-3xl perspective-container h-auto w-32  flex justify-center items-center relative ml-4">
-                  <div className="bg-violet-700 rounded-md perspective-element py-1 px-6 text-white  h-10 w-60 absolute"></div>
-                  <span className="w-auto absolute text-white ml-20">Interchange</span>
+    <div className=" overflow-x-hidden mt-16">
+      <div className="flex flex-col   items-center ">
+        <section className="w-full min-[80vh] max-w-7xl pt-20 relative ">
+          <div className="w-full h-[80vh] flex ">
+            <div className="w-1/2 h-full flex flex-col items-end justify-center  order-1 ">
+              {" "}
+              <span className="font-bold text-3xl w-[650px] pb-10 relative">
+                {" "}
+                <Image
+                  src={highlight}
+                  alt="highlight"
+                  width={40}
+                  height={0}
+                  className="absolute -top-5 -left-8"
+                />
+                NOSOTROS
+              </span>
+              <h3 className="text-3xl perspective-container max-w-[650px] font-bold leading-loose pb-2">
+                “ Transformando Sueños en Palabras, Palabras en Poder: <br />{" "}
+                <div className="flex ">
+                  creating{" "}
+                  <div className="text-3xl perspective-container h-auto w-32  flex justify-center items-center relative ml-4">
+                    <div className="bg-violet-700 rounded-md perspective-element py-1 px-6 text-white  h-10 w-60 absolute"></div>
+                    <span className="w-auto absolute text-white ml-20">Interchange</span>
+                  </div>
                 </div>
+                Donde el Ingles Abre Puertas”
+              </h3>
+              <div className="w-4 h-4 rounded-full bg-custom-yellow  absolute bottom-32 left-28"></div>
+              <div className="w-3 h-3 rounded-full bg-custom-green  absolute bottom-48 left-12"></div>
+            </div>
+            <div className="w-1/2 h-[80vh] order-2 relative">
+              {" "}
+              <Image
+                src={group}
+                alt="group"
+                width={0}
+                height={0}
+                className="absolute bottom-0 right-1/2 translate-x-[45%] z-10"
+              />
+              <div className=" h-[650px] w-[550px] absolute rounded-t-full bottom-0 right-1/2 translate-x-1/2 gradiente-opacidad">
+                {" "}
               </div>
-              Donde el Ingles Abre Puertas”
-            </h3>
-            <div className="w-4 h-4 rounded-full bg-custom-yellow  absolute bottom-32 left-28"></div>
-            <div className="w-3 h-3 rounded-full bg-custom-green  absolute bottom-48 left-12"></div>
+            </div>
           </div>
-          <div className="w-1/2 h-screen order-2"></div>
-        </div>
-      </section>
-      <section className="w-full min-h-screen pt-20 relative">
-        <div className="w-1/2 min-h-screen order-1"></div>
-        <div className="w-1/2 min-h-screen order-2"></div>
-      </section>
-    </>
+        </section>
+        <section className="w-full min-h-screen pt-20   relative  flex">
+          <div className="w-1/2 h-full order-1 relative">
+            <Image
+              src={label}
+              alt="label"
+              width={0}
+              height={0}
+              className="absolute -top-0 left-0"
+            />
+            <div className="grid grid-cols-1 border justify-items-center">
+              <Image
+                src={reactangle}
+                alt="photo 1"
+                width={0}
+                height={0}
+                className="-translate-x-1/3 translate-y-1/4 shadow-lg shadow-black/20 rounded-3xl"
+              />
+              <Image
+                src={rectangle1}
+                alt="photo 1"
+                width={0}
+                height={0}
+                className="translate-x-1/3 shadow-lg rounded-3xl shadow-black/20"
+              />
+              <Image
+                src={rectangle2}
+                alt="photo 1"
+                width={0}
+                height={0}
+                className="-translate-x-1/3 -translate-y-1/4 shadow-lg rounded-3xl shadow-black/20"
+              />
+              <Image
+                src={rectangle3}
+                alt="photo 1"
+                width={0}
+                height={0}
+                className="translate-x-1/3 -translate-y-1/2 shadow-lg rounded-3xl shadow-black/20"
+              />
+            </div>
+            <div className="w-5 h-5 rounded-full bg-custom-yellow absolute bottom-0 left-10"></div>
+            <div className="w-4 h-4 rounded-full bg-custom-orange  absolute bottom-10 left-20"></div>
+            <div className="w-3 h-3 rounded-full bg-custom-purple  absolute bottom-20 left-32"></div>
+          </div>
+          <div className="w-1/2 h-full order-2 relative border p-20 flex flex-col gap-4">
+            <h4 className="text-3xl text-center">Nuestro Instituto</h4>
+            <p className="text-lg pb-8">
+              Bienvenido a Interchange, tu destino para el aprendizaje del inglés de calidad. Nos
+              enorgullece ofrecer una experiencia educativa única y personalizada que va más allá de
+              las aulas tradicionales. Nuestra misión es inspirar y capacitar a nuestros estudiantes
+              para que alcancen la fluidez en inglés de manera efectiva y confiada.
+            </p>
+            <h5 className="text-3xl">Nuestra metodología</h5>
+            <p className="text-lg pb-8">
+              Adoptamos una metodología dinámica centrada en el aprendizaje activo y la aplicación
+              práctica del inglés en situaciones reales. Nuestro enfoque innovador, respaldado por
+              tecnología de vanguardia y profesionales cualificados, garantiza una experiencia de
+              aprendizaje efectiva y motivadora.
+            </p>
+            <h5 className="text-3xl">Nuestra Misión</h5>
+            <p className="text-lg pb-8">
+              Capacitar a nuestros estudiantes para lograr la fluidez en inglés de manera efectiva,
+              ofreciendo una experiencia educativa única y personalizada.
+            </p>
+            <h5 className="text-3xl">Nuestra Visión</h5>
+            <p className="text-lg pb-8">
+              Destacar como líderes en educación, proporcionando a nuestros estudiantes las
+              herramientas y la inspiración necesarias para sobresalir en un mundo globalizado
+              mediante el dominio del inglés, reconocidos por nuestra calidad e innovación.
+            </p>
+          </div>
+        </section>
+        <h3 className="text-5xl text-center max-w-[1000px] my-20">
+          Únete a nosotros y haz del aprendizaje una experiencia compartida
+        </h3>
+      </div>
+      <Banner />
+    </div>
   );
 };
 
--- client/app/components/footer/banner.tsx
+++ client/app/components/footer/banner.tsx
@@ -1,11 +1,42 @@
+"use client";
 import Image from "next/image";
 import React from "react";
 import teenphoto from "@/public/assets/svg/teenphoto.svg";
 import arrow2 from "@/public/assets/svg/arrow2.svg";
+import bannergroup from "@/public/assets/svg/bannergroup.png";
+import poliform from "@/public/assets/svg/poliform.png";
 
 export const Banner = () => {
+  const currentPath = typeof window !== "undefined" ? window.location.pathname : "/nosotros";
+  const imageHtml =
+    currentPath === "/nosotros" ? (
+      <div className="relative w-64">
+        <Image
+          src={bannergroup}
+          alt="teenphoto"
+          width={0}
+          height={0}
+          className="absolute bottom-0 -right-20 z-10 "
+        />
+        <Image
+          src={poliform}
+          alt="poliform"
+          width={0}
+          height={0}
+          className="absolute bottom-0 -right-20 scale-125 -translate-y-[12.5%]"
+        />
+      </div>
+    ) : (
+      <Image
+        src={teenphoto}
+        alt="teenphoto"
+        width={0}
+        height={0}
+        className="absolute bottom-0 -right-20"
+      />
+    );
   return (
-    <article className=" w-screen h-48 bg-gray-700 flex justify-center ">
+    <article className=" w-screen h-48  bg-gray-700 flex justify-center ">
       <div className="max-w-7xl w-full flex items-center ">
         <div className="flex justify-center items-center w-full h-full ">
           <h5 className="text-4xl w-2/3 text-white brightness-200">
@@ -23,15 +54,7 @@ export const Banner = () => {
             />
             <button className="yellow-btn">Register now</button>
           </div>
-          <div className="md:w-1/2 relative flex justify-center h-full">
-            <Image
-              src={teenphoto}
-              alt="teenphoto"
-              width={0}
-              height={0}
-              className="absolute bottom-0 -right-20"
-            />
-          </div>
+          <div className="md:w-1/2 relative flex justify-center h-full">{imageHtml}</div>
         </div>
       </div>
     </article>
