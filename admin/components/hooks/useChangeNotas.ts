a54890f9cd27f7964d151f0c895a40673be431f0
pablosecuen <pablosecuen@gmail.com>
2024-01-03 09:12:24 -0300

    custom hook for changing exam and trimesters grades

---
+++ admin/components/hooks/useChangeNotas.ts
@@ -0,0 +1,114 @@
+import React, { useEffect, useState } from "react";
+import { toast } from "sonner";
+
+interface Nota {
+    grado: string;
+    examenFinal: string;
+    trimestres: { trimestre: number; nota: string }[];
+  }
+
+const useChangeNotas = (alumno:any) => {
+    const [notasData, setNotasData] = useState<Nota>({
+        grado: "",
+        examenFinal: "",
+        trimestres: [],
+      });
+      const [gradoOptions, setGradoOptions] = useState<{ id: string; nombre: string }[]>([]);
+
+      useEffect(() => {
+        const gradosDisponibles = [
+          { id: "Starters", nombre: "Starters" },
+          { id: "Beginners", nombre: "Beginners" },
+          { id: "Pre-Kids", nombre: "preKids" },
+          { id: "Adol_1", nombre: "Adol_1" },
+          { id: "Adol_2", nombre: "Adol_2" },
+          { id: "Adol_3", nombre: "Adol_3" },
+          { id: "Adol_4", nombre: "Adol_4" },
+          { id: "Adol_5", nombre: "Adol_5" },
+          { id: "Kids_1", nombre: "Kids_1" },
+          { id: "Kids_2", nombre: "Kids_2" },
+          { id: "Kids_3", nombre: "Kids_3" },
+          { id: "Kids_4", nombre: "Kids_4" },
+          { id: "Kids_5", nombre: "Kids_5" },
+          { id: "Kids_6", nombre: "Kids_6" },
+          { id: "Adults_1", nombre: "Adults_1" },
+          { id: "Adults_2", nombre: "Adults_2" },
+          { id: "Adults_3", nombre: "Adults_3" },
+          { id: "Adults_4", nombre: "Adults_4" },
+          { id: "Adults_5", nombre: "Adults_5" },
+          { id: "Adults_6", nombre: "Adults_6" },
+          // ... otros grados disponibles
+        ];
+        setGradoOptions(gradosDisponibles);
+      }, []);
+
+      const handleSubmit = async () => {
+        try {
+          const response = await fetch(`http://localhost:3001/api/notas/${alumno.ID}`, {
+            method: "PATCH",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ Notas: [notasData] }),
+          });
+    
+toast.success("Notas actualizadas correctamente")
+          if (!response.ok) {
+            toast.error(`Error al actualizar las notas: ${response.statusText}`);
+            throw new Error("Error al actualizar las notas", );
+        }
+     } catch (error:any) {
+          console.error("Error al actualizar las notas:", error.message);
+        }
+      };
+
+      const handleTrimestreChange = (value: string) => {
+        const newTrimestre = { trimestre: parseInt(value), nota: "" };
+    
+        const existingTrimestreIndex = notasData.trimestres.findIndex(
+          (trimestre) => trimestre.trimestre === parseInt(value)
+        );
+    
+        if (existingTrimestreIndex === -1) {
+          setNotasData({
+            ...notasData,
+            trimestres: [...notasData.trimestres, newTrimestre],
+          });
+        }
+      };
+
+      const handleNotaChange = (value: string) => {
+        const lastIndex = notasData.trimestres.length - 1;
+    
+        if (lastIndex >= 0) {
+          const newTrimestres = [...notasData.trimestres];
+          newTrimestres[lastIndex] = {
+            ...newTrimestres[lastIndex],
+            nota: value,
+          };
+          setNotasData({ ...notasData, trimestres: newTrimestres });
+        }
+      };
+
+      const handleGradoChange = (value: string) => {
+        setNotasData({ ...notasData, grado: value });
+      };
+    
+      const handleExamenFinalChange = (value: string) => {
+        setNotasData({ ...notasData, examenFinal: value });
+      };
+
+
+  return {
+    notasData,
+    gradoOptions,
+    handleSubmit,
+    handleTrimestreChange,
+    handleNotaChange,
+    handleGradoChange,
+    handleExamenFinalChange,
+  };
+};
+
+
+export default useChangeNotas
--- admin/components/modal/modal-notas.tsx
+++ admin/components/modal/modal-notas.tsx
@@ -8,6 +8,7 @@ import {
   Input,
   ModalContent,
 } from "@nextui-org/react";
+import useChangeNotas from "../hooks/useChangeNotas";
 
 interface ModalNotasProps {
   onOpenChange: (value: boolean) => void;
@@ -22,101 +23,19 @@ interface Nota {
 }
 
 const CambiarNotasModal = ({ alumno, isOpen, onOpenChange }: ModalNotasProps) => {
-  const [notasData, setNotasData] = useState<Nota>({
-    grado: "",
-    examenFinal: "",
-    trimestres: [],
-  });
-  const [gradoOptions, setGradoOptions] = useState<{ id: string; nombre: string }[]>([]);
-  console.log(alumno);
 
-  useEffect(() => {
-    // Lógica para obtener los grados disponibles y establecer las opciones para el selector
-    // Ejemplo de datos de grados:
-    const gradosDisponibles = [
-      { id: "Starters", nombre: "Starters" },
-      { id: "Beginners", nombre: "Beginners" },
-      { id: "Pre-Kids", nombre: "preKids" },
-      { id: "Adol_1", nombre: "Adol_1" },
-      { id: "Adol_2", nombre: "Adol_2" },
-      { id: "Adol_3", nombre: "Adol_3" },
-      { id: "Adol_4", nombre: "Adol_4" },
-      { id: "Adol_5", nombre: "Adol_5" },
-      { id: "Kids_1", nombre: "Kids_1" },
-      { id: "Kids_2", nombre: "Kids_2" },
-      { id: "Kids_3", nombre: "Kids_3" },
-      { id: "Kids_4", nombre: "Kids_4" },
-      { id: "Kids_5", nombre: "Kids_5" },
-      { id: "Kids_6", nombre: "Kids_6" },
-      { id: "Adults_1", nombre: "Adults_1" },
-      { id: "Adults_2", nombre: "Adults_2" },
-      { id: "Adults_3", nombre: "Adults_3" },
-      { id: "Adults_4", nombre: "Adults_4" },
-      { id: "Adults_5", nombre: "Adults_5" },
-      { id: "Adults_6", nombre: "Adults_6" },
-      // ... otros grados disponibles
-    ];
-    setGradoOptions(gradosDisponibles);
-  }, []);
+  const {
+    notasData,
+    gradoOptions,
+    handleSubmit,
+    handleTrimestreChange,
+    handleNotaChange,
+    handleGradoChange,
+    handleExamenFinalChange,
+  } = useChangeNotas(alumno);
 
-  // Función para guardar las notas actualizadas
-  const handleSubmit = async () => {
-    try {
-      const response = await fetch(`http://localhost:3001/api/notas/${alumno.ID}`, {
-        method: "PATCH",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ Notas: [notasData] }),
-      });
-
-      if (response.ok) {
-        // Realizar alguna acción después de una solicitud exitosa (por ejemplo, cerrar el modal)
-      } else {
-        console.error("Error al actualizar las notas:", response.statusText);
-      }
-    } catch (error: any) {
-      console.error("Error al actualizar las notas:", error.message);
-    }
-  };
 
   const trimestresDisponibles = [1, 2, 3, 4];
-
-  const handleTrimestreChange = (value: string) => {
-    const newTrimestre = { trimestre: parseInt(value), nota: "" };
-
-    const existingTrimestreIndex = notasData.trimestres.findIndex(
-      (trimestre) => trimestre.trimestre === parseInt(value)
-    );
-
-    if (existingTrimestreIndex === -1) {
-      setNotasData({
-        ...notasData,
-        trimestres: [...notasData.trimestres, newTrimestre],
-      });
-    }
-  };
-
-  const handleNotaChange = (value: string) => {
-    const lastIndex = notasData.trimestres.length - 1;
-
-    if (lastIndex >= 0) {
-      const newTrimestres = [...notasData.trimestres];
-      newTrimestres[lastIndex] = {
-        ...newTrimestres[lastIndex],
-        nota: value,
-      };
-      setNotasData({ ...notasData, trimestres: newTrimestres });
-    }
-  };
-
-  const handleGradoChange = (value: string) => {
-    setNotasData({ ...notasData, grado: value });
-  };
-
-  const handleExamenFinalChange = (value: string) => {
-    setNotasData({ ...notasData, examenFinal: value });
-  };
 
   return (
     <div className="flex justify-center items-center w-full h-full">
