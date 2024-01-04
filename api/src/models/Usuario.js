0edbe096c8e5979ac184a041ae659f8e6b8fe70b
pablosecuen <pablosecuen@gmail.com>
2024-01-04 11:58:22 -0300

    modelo usuarios para incorporar informacion de adultos responsables

--- api/src/models/Usuario.js
+++ api/src/models/Usuario.js
@@ -12,36 +12,69 @@ module.exports = (sequelize) => {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
-        notEmpty: true,
+        notEmpty: {
+          msg: "El nombre es obligatorio",
+        },
+        len: {
+          args: [2, 50],
+          msg: "El nombre debe tener entre 2 y 50 caracteres",
+        },
       },
     },
     Apellido: {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
-        notEmpty: true,
+        notEmpty: {
+          msg: "El apellido es obligatorio",
+        },
+        len: {
+          args: [2, 50],
+          msg: "El apellido debe tener entre 2 y 50 caracteres",
+        },
       },
     },
     Telefono: {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
-        notEmpty: true,
+        notEmpty: {
+          msg: "El teléfono es obligatorio",
+        },
+        len: {
+          args: [7, 15],
+          msg: "El teléfono debe tener entre 7 y 15 caracteres",
+        },
       },
     },
     Email: {
       type: DataTypes.STRING,
       allowNull: false,
-      unique: true,
+      unique: {
+        args: true,
+        msg: "El email ya está en uso",
+      },
       validate: {
-        isEmail: true,
+        isEmail: {
+          msg: "El email debe ser válido",
+        },
+        len: {
+          args: [5, 255],
+          msg: "El email debe tener entre 5 y 255 caracteres",
+        },
       },
     },
     Password: {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
-        notEmpty: true,
+        notEmpty: {
+          msg: "La contraseña es obligatoria",
+        },
+        len: {
+          args: [6, 100],
+          msg: "La contraseña debe tener entre 6 y 100 caracteres",
+        },
       },
     },
     Anotaciones: {
@@ -51,16 +84,63 @@ module.exports = (sequelize) => {
     Tipo: {
       type: DataTypes.STRING,
       validate: {
-        notEmpty: true,
+        notEmpty: {
+          msg: "El tipo es obligatorio",
+        },
       },
     },
     Activo: {
       type: DataTypes.BOOLEAN,
       allowNull: false,
+      defaultValue: true,
     },
     Grado_ID: {
       type: DataTypes.UUID,
       allowNull: true,
+    },
+    // Nuevos campos para adultos responsables
+    NombreAdulto: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      validate: {
+        len: {
+          args: [2, 50],
+          msg: "El nombre del adulto debe tener entre 2 y 50 caracteres",
+        },
+      },
+    },
+    ApellidoAdulto: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      validate: {
+        len: {
+          args: [2, 50],
+          msg: "El apellido del adulto debe tener entre 2 y 50 caracteres",
+        },
+      },
+    },
+    TelefonoAdulto: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      validate: {
+        len: {
+          args: [7, 15],
+          msg: "El teléfono del adulto debe tener entre 7 y 15 caracteres",
+        },
+      },
+    },
+    EmailAdulto: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      validate: {
+        isEmail: {
+          msg: "El email del adulto debe ser válido",
+        },
+        len: {
+          args: [5, 255],
+          msg: "El email del adulto debe tener entre 5 y 255 caracteres",
+        },
+      },
     },
   });
 
