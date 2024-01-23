a13bb89af3fe3db1fa790fc28d82c014ccb92b8c
pablosecuen <pablosecuen@gmail.com>
2024-01-23 19:33:40 -0300

    debbugging deploy cors policies

--- api/app.js
+++ api/app.js
@@ -29,6 +29,7 @@ app.use((req, res, next) => {
   if (allowedOrigins.includes(origin)) {
     res.setHeader("Access-Control-Allow-Origin", origin);
   }
+  res.header("Access-Control-Allow-Credentials", true);
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   // Handling preflight requests
