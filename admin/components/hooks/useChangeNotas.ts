f0eb7cf1312991babc7cf3974eb8aa4a13cf7163
pablosecuen <pablosecuen@gmail.com>
2024-01-24 23:03:19 -0300

    type fix

--- admin/components/hooks/useChangeNotas.ts
+++ admin/components/hooks/useChangeNotas.ts
@@ -3,7 +3,7 @@ import { toast } from "sonner";
 import { baseUrl } from "./baseurl";
 
 export interface Nota {
-  notas: any;
+  notas?: any;
   grado: string;
   examenFinal: string;
   trimestres: {
