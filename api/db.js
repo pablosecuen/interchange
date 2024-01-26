ceffe5db3bc06f952137c531f61b46f19f6481f1
pablosecuen <pablosecuen@gmail.com>
2024-01-26 15:08:02 -0300

    fix fb for deploy with railway

--- api/db.js
+++ api/db.js
@@ -3,13 +3,11 @@ const { Sequelize } = require("sequelize");
 const dotenv = require("dotenv");
 dotenv.config();
 
-let DATABASE_NAME = process.env.DATABASE_NAME;
-let DATABASE_USERNAME = process.env.DATABASE_USERNAME;
-let DATABASE_PASSWORD = process.env.DATABASE_PASSWORD;
 
-if (DATABASE_NAME && DATABASE_USERNAME && DATABASE_PASSWORD) {
-  const sequelize = new Sequelize(DATABASE_NAME, DATABASE_USERNAME, DATABASE_PASSWORD, {
-    host: "localhost",
+const DATABASE_URL = process.env.RAILWAY_DATABASE_URL;
+
+if (DATABASE_URL) {
+  const sequelize = new Sequelize(DATABASE_URL, {
     dialect: "postgres",
     logging: false,
   });
@@ -65,6 +63,6 @@ if (DATABASE_NAME && DATABASE_USERNAME && DATABASE_PASSWORD) {
   Campus.belongsTo(Grado, { foreignKey: "Grado_ID" });
   Grado.hasMany(Campus, { foreignKey: "Grado_ID" });
 } else {
-  console.error("Las variables de entorno de la base de datos no están completamente definidas.");
+  console.error("La variable de entorno RAILWAY_DATABASE_URL no está definida.");
   process.exit(1);
 }
