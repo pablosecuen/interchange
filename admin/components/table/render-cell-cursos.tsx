6951c5c03aeea5ba51cea290bd8dded421956d65
pablosecuen <pablosecuen@gmail.com>
2024-01-26 14:35:04 -0300

    cleaned tableCursos component by passing logic into de modal

--- admin/components/table/render-cell-cursos.tsx
+++ admin/components/table/render-cell-cursos.tsx
@@ -1,16 +1,14 @@
-import { User, Tooltip, Chip, Button } from "@nextui-org/react";
+import { User, Tooltip } from "@nextui-org/react";
 import React from "react";
-import { DeleteIcon } from "../icons/table/delete-icon";
-import { EditIcon } from "../icons/table/edit-icon";
-import { EyeIcon } from "../icons/table/eye-icon";
+
+import ModalCurso from "../modal/modal-cursos";
 
 interface Props {
   curso: any;
   columnKey: string | React.Key;
-  handleSelectCurso: (curso: any) => void;
 }
 
-export const RenderCell = ({ curso, columnKey, handleSelectCurso }: Props) => {
+export const RenderCell = ({ curso, columnKey }: Props) => {
   // @ts-ignore
 
   const cellValue = curso[columnKey];
@@ -30,27 +28,7 @@ export const RenderCell = ({ curso, columnKey, handleSelectCurso }: Props) => {
     case "actions":
       return (
         <div className="flex items-center gap-4 ">
-          <div onClick={() => handleSelectCurso(curso)}>
-            <Tooltip content="Details">
-              <button onClick={() => handleSelectCurso(curso)}>
-                <EyeIcon size={20} fill="#979797" />
-              </button>
-            </Tooltip>
-          </div>
-          <div>
-            <Tooltip content="Editar curso" color="secondary">
-              <button onClick={() => console.log(curso)}>
-                <EditIcon size={20} fill="#979797" />
-              </button>
-            </Tooltip>
-          </div>
-          <div>
-            <Tooltip content="Borrar curso" color="danger">
-              <button onClick={() => console.log("Delete curso", curso.ID)}>
-                <DeleteIcon size={20} fill="#FF0080" />
-              </button>
-            </Tooltip>
-          </div>
+          <ModalCurso curso={curso} />
         </div>
       );
     default:
