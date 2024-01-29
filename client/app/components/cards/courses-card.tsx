e17dd221fe5c9f12c6799d30099ad31cd4bdf5f7
pablosecuen <pablosecuen@gmail.com>
2024-01-29 13:36:01 -0300

    asd

--- client/app/components/cards/courses-card.tsx
+++ client/app/components/cards/courses-card.tsx
@@ -24,7 +24,7 @@ const Card = ({ course }: any) => {
       <div className="p-2 flex flex-col gap-1  h-1/3">
         <h4 className="font-bold">{title}</h4>
         <p className="text-xs text-gray-400">{description}</p>
-        <Link href={url} className="pt-3">
+        <Link href="/cursos" className="pt-3">
           <button className="text-custom-yellow hover:text-yellow-600 text-sm tracking-widest">
             leer mas
           </button>
