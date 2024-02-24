2c5bce748c37b09f0f40a14a5838811d773bba4e
pablosecuen <pablosecuen@gmail.com>
2024-02-23 21:53:19 -0300

    fix

--- client/app/(...)/inscripcion/page.tsx
+++ client/app/(...)/inscripcion/page.tsx
@@ -81,7 +81,7 @@ const Inscripcion = () => {
               htmlFor="floating_last_name"
               className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-purple-600 peer-focus:dark:text-purple-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
             >
-              Apellido de adulto 2
+              Apellido de adulto 1
             </label>
           </div>
         </div>
--- client/app/components/login/register.tsx
+++ client/app/components/login/register.tsx
@@ -254,7 +254,7 @@ const Register = ({ onClose, toggleRegister, updateUser }: RegisterProps) => {
               htmlFor="phoneAdult"
               className="peer-focus:font-medium absolute text-xs text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-custom-yellow peer-focus:dark:text-custom-yellow peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
             >
-              Telefono Adulto
+              Telefono Adulto 1
             </label>
           </div>
           <div className="relative z-0 w-full mb-5 group">
