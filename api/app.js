ec66e484f6d13a26bbc946d0a53b4b4108e78036
pablosecuen <pablosecuen@gmail.com>
2024-03-01 11:09:29 -0300

    basurl for main developerment enviroment

--- api/app.js
+++ api/app.js
@@ -22,12 +22,9 @@ const app = express();
 // Middleware para habilitar CORS
 app.use((req, res, next) => {
   const allowedOrigins = [
-     "http://localhost:3000",
-    "http://localhost:3002",
+    "http://localhost:3000",
     "http://localhost:3001",
-    "https://interchange-admin.vercel.app",
-    "https://interchange-azure.vercel.app",
-    "https://www.interchange.com.ar",
+    "http://localhost:3002",
   ];
   const origin = req.headers.origin;
   if (allowedOrigins.includes(origin)) {
