ebe942cbf0cb022f1aeb1abc01f1806316ed417c
pablosecuen <pablosecuen@gmail.com>
2024-03-01 11:58:39 -0300

    fix cors

--- api/app.js
+++ api/app.js
@@ -32,6 +32,8 @@ app.use((req, res, next) => {
   const origin = req.headers.origin;
   if (allowedOrigins.includes(origin)) {
     res.setHeader("Access-Control-Allow-Origin", origin);
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   }
 
   // Handling preflight requests
