14a7b604ce6453e1c3ce000dc40c29225ea3e46c
pablosecuen <pablosecuen@gmail.com>
2024-03-01 20:26:51 -0300

    PATCH method

--- api/app.js
+++ api/app.js
@@ -33,7 +33,7 @@ app.use((req, res, next) => {
   const origin = req.headers.origin;
   if (allowedOrigins.includes(origin)) {
     res.setHeader("Access-Control-Allow-Origin", origin);
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS,PATCH");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   }
 
