b194e235edbc12f54e72c3c15ed58b602a33d219
pablosecuen <pablosecuen@gmail.com>
2024-03-07 09:42:20 -0300

    .

--- api/app.js
+++ api/app.js
@@ -22,7 +22,7 @@ const app = express();
 // Middleware para habilitar CORS
 app.use((req, res, next) => {
   const allowedOrigins = [
-       "http://localhost:3000",
+    "http://localhost:3000",
     "http://localhost:3002",
     "http://localhost:3001",
     "https://interchange-admin.vercel.app",
@@ -32,7 +32,7 @@ app.use((req, res, next) => {
 
   const origin = req.headers.origin;
   if (allowedOrigins.includes(origin)) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
+    res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS,PATCH");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   }
