9e75e4a1ada5c11c174d399c5bd299904a6eb09f
pablosecuen <pablosecuen@gmail.com>
2024-03-07 09:48:04 -0300

    preflight config

--- api/app.js
+++ api/app.js
@@ -39,6 +39,9 @@ app.use((req, res, next) => {
 
   // Handling preflight requests
   if (req.method === "OPTIONS") {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, PATCH");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
     res.sendStatus(200);
   } else {
     next();
