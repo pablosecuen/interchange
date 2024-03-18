dc8ebd71967d260547ce55f3c301bc46fb6991a4
pablosecuen <pablosecuen@gmail.com>
2024-03-18 20:43:06 -0300

    fix register

--- client/app/components/login/register.tsx
+++ client/app/components/login/register.tsx
@@ -262,7 +262,7 @@ const Register = ({ onClose, toggleRegister, updateUser }: RegisterProps) => {
               type="text"
               id="phoneAdult2"
               name="phoneAdult2"
-              value={formData.phoneAdult}
+              value={formData.phoneAdult2}
               onChange={handleChange}
               className="block py-2.5 px-0 w-full h-auto text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-custom-yellow focus:outline-none focus:ring-0 focus:border-custom-yellow peer "
               placeholder=" "
